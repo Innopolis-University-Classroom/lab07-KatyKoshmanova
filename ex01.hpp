05dc5f391af810e67e6901dfb2e5c41d56a1142a
katykoshmanova <katykosh@yandex.ru>
2022-03-07 16:55:04 +0300

    k1

--- ex01.hpp
+++ ex01.hpp
@@ -2,7 +2,10 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-void multiples (int& sum, int x, int n)
-{
-//kkjkjkjkjj
+template <class T>
+void multiples (T& sum, T x, int n) {
+    sum += 1;
+    for (int i = 0; i < n; i++){
+        sum += x*n;
+    }
 }
